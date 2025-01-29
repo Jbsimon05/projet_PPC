082ee3011e7635d87b5fac24db45b7043289891d
Jean-Baptiste SIMON <72874867+Jbsimon05@users.noreply.github.com>
2025-01-29 09:56:34 +0100

    t_feux + sirenes NS/EW + correction lights inachevée + doc

--- lights.py
+++ lights.py
@@ -1,19 +1,19 @@
 import time
 
-def lights_manager(traffic_lights, signal_event) :
+#temps de maintien au vert des feux en s
+t_feux = 6
+
+def lights_manager(traffic_lights, sirene_NS, sirene_EW) :
     """
     [North, South, East, West]
     traffic_lights = [1, 1, 0, 0] or [0, 0, 1, 1]
     """
     while True :
-        if signal_event.is_set():
+        if sirene_NS.is_set():
+            print("Pin pon pin pon...")
             print("High-priority vehicle detected! Adjusting lights...")
-            # Adjust the lights to allow priority vehicle to pass
-            if traffic_lights == [1, 1, 0, 0]:
-                traffic_lights[:] = [0, 0, 1, 1]
-            else:
-                traffic_lights[:] = [1, 1, 0, 0]
-            signal_event.clear()
+            traffic_lights[:] = [1, 1, 0, 0]
+            sirene_NS.clear()
             time.sleep(6)
         else :
             if traffic_lights == [1, 1, 0, 0] :
