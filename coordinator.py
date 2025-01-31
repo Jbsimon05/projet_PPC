2a65d07944472e9f603a84a6a390ca5389d864f0
Jean-Baptiste SIMON <72874867+Jbsimon05@users.noreply.github.com>
2025-01-31 10:23:23 +0100

    debug coord

--- coordinator.py
+++ coordinator.py
@@ -16,7 +16,7 @@ def coordinator_process(north, south, east, west, bouchons, traffic_lights) :
                 bouchons[0] -= 1
                 print(f"Processing vehicule : {vehicule1}")
                 time.sleep(t_pass)
-            elif not south.empty:
+            if not south.empty:
                 #1er véhicule au sud passe
                 vehicule2 = south.get()
                 bouchons[1] -= 1
@@ -30,7 +30,7 @@ def coordinator_process(north, south, east, west, bouchons, traffic_lights) :
                 bouchons[2] -= 1
                 print(f"Processing vehicule : {vehicule1}")
                 time.sleep(t_pass)
-            elif not west.empty:
+            if not west.empty:
                 #1er véhicule à l'ouest passe
                 vehicule2 = west.get()
                 bouchons[3] -= 1
